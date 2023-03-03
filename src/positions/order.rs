434f17c77a14061702b0a7020229a2fc06922b84
Maxim Gorin <maksim.g@simple.app>
2023-03-03 18:08:29 +0300

    Refactoring

--- src/positions/order.rs
+++ src/positions/order.rs
@@ -1,31 +1,31 @@
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct OrderSbModel {
     #[prost(string, tag = "1")]
-    pub id: ::prost::alloc::string::String,
+    pub id: String,
     #[prost(string, tag = "2")]
-    pub trader_id: ::prost::alloc::string::String,
+    pub trader_id: String,
     #[prost(string, tag = "3")]
-    pub wallet_id: ::prost::alloc::string::String,
+    pub wallet_id: String,
     #[prost(string, tag = "4")]
-    pub instrument: ::prost::alloc::string::String,
+    pub instrument: String,
     #[prost(string, tag = "5")]
-    pub base_asset: ::prost::alloc::string::String,
+    pub base_asset: String,
     #[prost(double, tag = "6")]
     pub leverage: f64,
     #[prost(enumeration = "OrderSideSb", tag = "7")]
     pub side: i32,
     #[prost(message, optional, tag = "8")]
-    pub take_profit: ::core::option::Option<AutoCloseConfigSbModel>,
+    pub take_profit: Option<AutoCloseConfigSbModel>,
     #[prost(message, optional, tag = "9")]
-    pub stop_loss: ::core::option::Option<AutoCloseConfigSbModel>,
+    pub stop_loss: Option<AutoCloseConfigSbModel>,
     #[prost(double, tag = "10")]
     pub stop_out_percent: f64,
     #[prost(double, tag = "11")]
     pub margin_call_percent: f64,
     #[prost(map = "string, double", tag = "12")]
-    pub invest_assets: ::std::collections::HashMap<::prost::alloc::string::String, f64>,
+    pub invest_assets: std::collections::HashMap<String, f64>,
     #[prost(message, optional, tag = "13")]
-    pub desire_price: ::core::option::Option<f64>,
+    pub desire_price: Option<f64>,
     #[prost(bool, tag = "14")]
     pub top_up_enabled: bool,
     #[prost(double, tag = "15")]
@@ -44,21 +44,21 @@ pub struct AutoCloseConfigSbModel {
     pub unit: i32,
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, prost::Enumeration)]
 #[repr(i32)]
 pub enum OrderSideSb {
     Buy = 0,
     Sell = 1,
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, prost::Enumeration)]
 #[repr(i32)]
 pub enum OrderTypeSb {
     Market = 0,
     Limit = 1,
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, prost::Enumeration)]
 #[repr(i32)]
 pub enum AutoCloseUnitSb {
     AssetAmount = 0,
