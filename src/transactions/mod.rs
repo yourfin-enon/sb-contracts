785a9ba79e1a2973d6baba78328b14b860af11d0
Maxim Gorin <maksim.g@simple.app>
2023-04-25 13:24:46 +0300

    Add transaction updated event

--- src/transactions/mod.rs
+++ src/transactions/mod.rs
@@ -1,2 +1,3 @@
 pub mod transaction_received;
 pub mod shared;
+pub mod transaction_updated;
---
+++ src/transactions/transaction_updated.rs
@@ -0,0 +1,87 @@
+use std::collections::HashMap;
+use my_service_bus_abstractions::{GetMySbModelTopicId, SubscriberError};
+use my_service_bus_abstractions::publisher::MySbMessageSerializer;
+use my_service_bus_abstractions::subscriber::MySbMessageDeserializer;
+
+pub const TOPIC_NAME: &str = "transaction-updated";
+
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct TransactionUpdatedSbEvent {
+    #[prost(int64, tag = "1")]
+    pub date_micros: i64,
+    #[prost(string, tag = "2")]
+    pub asset_symbol: String,
+    #[prost(string, tag = "3")]
+    pub tx_id: String,
+    #[prost(string, tag = "4")]
+    pub destination_address: String,
+    #[prost(string, optional, tag = "5")]
+    pub source_address: Option<String>,
+    #[prost(string, tag = "6")]
+    pub wallet_id: String,
+    #[prost(int32, tag = "7")]
+    pub transaction_type: i32,
+    #[prost(double, tag = "8")]
+    pub asset_amount: f64,
+    #[prost(string, tag = "9")]
+    pub trader_id: String,
+    #[prost(string, tag = "10")]
+    pub blockchain_symbol: String,
+    #[prost(double, tag = "11")]
+    pub fee_amount: f64,
+    #[prost(int32, tag = "12")]
+    pub status: i32,
+    #[prost(string, tag = "13")]
+    pub external_id: String,
+    #[prost(int32, tag = "14")]
+    pub payment_provider: i32,
+    #[prost(string, tag = "15")]
+    pub id: String,
+}
+
+impl TransactionUpdatedSbEvent {
+    fn as_bytes(&self) -> Result<Vec<u8>, prost::EncodeError> {
+        let mut result = Vec::new();
+        prost::Message::encode(self, &mut result)?;
+        Ok(result)
+    }
+
+    fn from_bytes(bytes: &[u8]) -> Result<Self, prost::DecodeError> {
+        prost::Message::decode(bytes)
+    }
+}
+
+impl GetMySbModelTopicId for TransactionUpdatedSbEvent {
+    fn get_topic_id() -> &'static str {
+        TOPIC_NAME
+    }
+}
+
+impl MySbMessageSerializer for TransactionUpdatedSbEvent {
+    fn serialize(
+        &self,
+        headers: Option<std::collections::HashMap<String, String>>,
+    ) -> Result<(Vec<u8>, Option<std::collections::HashMap<String, String>>), String> {
+        let content = self.as_bytes();
+
+        return match content {
+            Ok(content) => Ok((content, headers)),
+            Err(err) => Err(format!("{err}")),
+        }
+    }
+}
+
+impl MySbMessageDeserializer for TransactionUpdatedSbEvent {
+    type Item = TransactionUpdatedSbEvent;
+    fn deserialize(
+        src: &[u8],
+        _headers: &Option<HashMap<String, String>>,
+    ) -> Result<Self::Item, SubscriberError> {
+        let result = TransactionUpdatedSbEvent::from_bytes(src);
+
+        return match result {
+            Ok(model) => Ok(model),
+            Err(err) => Err(SubscriberError::CanNotDeserializeMessage(format!("{err}"))),
+        }
+    }
+}
