dfc374c3705ecf7f1056158ce21850eb763b0c31
Maxim Gorin <maksim.g@simple.app>
2023-05-19 14:44:16 +0300

    Change 2fa model

--- src/verifications/two_fa_settings_updated.rs
+++ src/verifications/two_fa_settings_updated.rs
@@ -10,6 +10,10 @@ pub struct TwoFaSettingsUpdatedSbModel {
     pub trader_id: String,
     #[prost(bool, optional, tag = "2")]
     pub google_code_enabled: Option<bool>,
+    #[prost(string, tag = "3")]
+    pub device: String,
+    #[prost(string, tag = "4")]
+    pub ip: String,
 }
 
 impl TwoFaSettingsUpdatedSbModel {
