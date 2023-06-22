fa3f45847f8d27374a9afd700d7c84a5c99e0895
Maxim Gorin <maksim.g@simple.app>
2023-06-22 21:40:12 +0300

    Update top-up event

--- src/positions/position_top_up.rs
+++ src/positions/position_top_up.rs
@@ -14,13 +14,13 @@ pub struct PositionTopUpSbEvent {
     #[prost(message, optional, tag = "2")]
     pub order: Option<OrderSbModel>,
     #[prost(message, repeated, tag = "3")]
-    pub top_ups: Vec<TopUpSbModel>,
+    pub active_top_ups: Vec<ActiveTopUpSbModel>,
     #[prost(message, repeated, tag = "4")]
-    pub canceled_top_ups: Vec<TopUpSbModel>,
+    pub canceled_top_ups: Vec<CanceledTopUpSbModel>,
 }
 
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct TopUpSbModel {
+pub struct ActiveTopUpSbModel {
     #[prost(string, tag = "1")]
     pub id: String,
     #[prost(int64, tag = "2")]
@@ -31,6 +31,24 @@ pub struct TopUpSbModel {
     pub instrument_price: f64,
     #[prost(map = "string, double", tag = "5")]
     pub asset_prices: HashMap<String, f64>,
+}
+
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct CanceledTopUpSbModel {
+    #[prost(string, tag = "1")]
+    pub id: String,
+    #[prost(int64, tag = "2")]
+    pub date_micros: i64,
+    #[prost(map = "string, double", tag = "3")]
+    pub assets: HashMap<String, f64>,
+    #[prost(double, tag = "4")]
+    pub instrument_price: f64,
+    #[prost(map = "string, double", tag = "5")]
+    pub asset_prices: HashMap<String, f64>,
+    #[prost(double, tag = "6")]
+    pub cancel_instrument_price: f64,
+    #[prost(int64, tag = "7")]
+    pub cancel_date_micros: i64,
 }
 
 impl PositionTopUpSbEvent {
