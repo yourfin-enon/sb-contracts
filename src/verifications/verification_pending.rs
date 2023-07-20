e28b9ce41936df66d90ce02ee89140375aa179d9
Maxim Gorin <maksim.g@simple.app>
2023-07-20 14:54:07 +0300

    Change verification

--- src/verifications/verification_pending.rs
+++ src/verifications/verification_pending.rs
@@ -16,6 +16,8 @@ pub struct VerificationPendingSbModel {
     pub reason: i32,
     #[prost(optional, string, tag = "5")]
     pub additional_data_json: Option<String>,
+    #[prost(int32, tag = "6")]
+    pub lifetime_sec: i32,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
