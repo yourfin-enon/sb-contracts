4798474ef96e615d342b11b0cf8bde01ce8e47c4
Maxim Gorin <onlinexmax@gmail.com>
2024-04-30 19:41:04 +0300

    Add transaction convert

--- src/transactions/mod.rs
+++ src/transactions/mod.rs
@@ -1,3 +1,4 @@
 pub mod transaction_received;
 pub mod shared;
 pub mod transaction_updated;
+pub mod transaction_converted;
---
+++ src/transactions/transaction_converted.rs
@@ -0,0 +1,24 @@
+service_sdk::macros::use_my_sb_entity_protobuf_model!();
+
+#[derive(Clone, PartialEq, ::prost::Message)]
+#[my_sb_entity_protobuf_model(topic_id = "transaction-converted")]
+pub struct TransactionConvertedSbEvent {
+    #[prost(string, tag = "1")]
+    pub convert_id: String,
+    #[prost(string, tag = "2")]
+    pub trader_id: String,
+    #[prost(int64, tag = "3")]
+    pub create_date_millis: i64,
+    #[prost(string, tag = "4")]
+    pub fiat_asset: String,
+    #[prost(string, tag = "5")]
+    pub crypto_asset: String,
+    #[prost(double, tag = "6")]
+    pub fiat_amount: f64,
+    #[prost(double, tag = "7")]
+    pub crypto_amount: f64,
+    #[prost(string, tag = "8")]
+    pub transaction_id: String,
+}
+
+
