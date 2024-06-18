67c1ca7aefbd8b612295d91373f8257f0fdfc932
Maxim Gorin <onlinexmax@gmail.com>
2024-06-18 17:03:43 +0300

    Fix build

--- src/trader/registered_event.rs
+++ src/trader/registered_event.rs
@@ -5,6 +5,6 @@ service_sdk::macros::use_my_sb_entity_protobuf_model!();
 pub struct TraderRegisteredSbEvent {
     #[prost(string, tag = "1")]
     pub trader_id: String,
-    #[prost(string, tag = "2")]
+    #[prost(optional, string, tag = "2")]
     pub fingerprint: Option<String>,
 }
